168180a2c7bd32c7e9f6eef721f3767ceb04d94a
alrobinson14 <alrobinson@knights.ucf.edu>
2017-05-31 18:03:21 -0600

    Another way to compute stats
    A more readable, less efficient way. The point was to do this problem
    again with another way to come up with the same answer. First answer was
    better.

--- src/Functions/simple_stats.m
+++ src/Functions/simple_stats.m
@@ -1,5 +1,16 @@
 function S = simple_stats(N)
+% simple_stats is a function that takes in a matrix N and will calculate statistics, which will output the matrix S
+% First column  = mean
+% Second column = median
+% Third column  = minimums
+% Fourth column = maximums
+    
 
-    S = [mean(N,2),median(N,2),min(N,[],2),max(N,[],2)];
+    A = mean(N,2);
+    B = median(N,2);
+    C = min(N,[],2);
+    D = max(N,[],2);
+    S = [A B C D];
+
     
 end
